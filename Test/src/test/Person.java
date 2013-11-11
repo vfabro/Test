7736f5b7882adf18223002a9d4bd2a82f8a9f9d1
vincent <vfabro@gmail.com>
2013-11-11 18:39:54 +0100

    Add Person

---
+++ Test/src/test/Person.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Person {
+	
+	private String _name;
+	private int _age;
+	
+	public String getName() {
+		return _name;
+	}
+	public void setName(String name) {
+		_name = name;
+	}
+	public int getAge() {
+		return _age;
+	}
+	public void setAge(int age) {
+		_age = age;
+	}
+
+}
